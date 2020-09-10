95c9c3a387cf547cdf89cad20fa4086c3715bcae
Raymond Lian <r.lian@ieee.org>
2020-09-10 15:47:30 -0400

    using a different implementation for calling the inAppReview, solution came from https://stackoverflow.com/questions/63286540/play-core-in-app-review-api-not-showing-the-review-activity. Tested implementation seems to have success but must test in beta to see if it will work properly

--- src/android/AppRate.java
+++ src/android/AppRate.java
@@ -1,98 +1,120 @@
 package org.pushandplay.cordova.apprate;
 
+import org.apache.cordova.*;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
 import android.content.pm.ApplicationInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManagerFactory;
 import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.tasks.OnCompleteListener;
+import com.google.android.play.core.tasks.OnFailureListener;
 import com.google.android.play.core.tasks.Task;
 
 public class AppRate extends CordovaPlugin {
-	private ReviewManager manager;
+  ReviewManager manager;
+  ReviewInfo reviewInfo;
 
-	private final String GETAPPVERSION = "getAppVersion";
-	private final String GETAPPTITLE = "getAppTitle";
-	private final String LAUNCHREVIEW = "launchAndroidReview";
-	@Override
-	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException
-	{
 
-		try {
-			PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-			this.manager = ReviewManagerFactory.create(this.cordova.getActivity());
+  private final String GETAPPVERSION = "getAppVersion";
+  private final String GETAPPTITLE = "getAppTitle";
+  private final String LAUNCHREVIEW = "launchAndroidReview";
 
-			switch(action){
-				case GETAPPVERSION:
-					this.getAppVersion(callbackContext, packageManager);
-				break;
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
 
-				case GETAPPTITLE:
-					this.getAppTitle(callbackContext, packageManager);
-				break;
+  @Override
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException
+  {
+    Log.v("APPRATE:::", action);
+    try {
+      PackageManager packageManager = cordova.getActivity().getPackageManager();
+      this.manager = ReviewManagerFactory.create(cordova.getActivity());
 
-				case LAUNCHREVIEW:
-					this.requestReviewInfo();
-				break;
-			}
-			return false;
-		} catch (Exception e) {
+      switch(action){
+        case GETAPPVERSION:
+          getAppVersion(callbackContext, packageManager);
+          break;
+
+        case GETAPPTITLE:
+          getAppTitle(callbackContext, packageManager);
+          break;
+
+        case LAUNCHREVIEW:
+          Log.v("CASE:::", LAUNCHREVIEW);
+          //requestReviewInfo();
+          reviewInApp();
+          break;
+      }
+      return false;
+    } catch (Exception e) {
       e.printStackTrace();
       return true;
     }
   }
 
-	private boolean getAppVersion(CallbackContext callbackContext, PackageManager packageManager){
-	  try{
-      callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionName);
+  private boolean getAppVersion(CallbackContext callbackContext, PackageManager packageManager){
+    try{
+      callbackContext.success(packageManager.getPackageInfo(cordova.getActivity().getPackageName(), 0).versionName);
     } catch (NameNotFoundException e) {
       e.printStackTrace();
     }
 
     return true;
-	}
-	private boolean getAppTitle(CallbackContext callbackContext, PackageManager packageManager){
+  }
+  private boolean getAppTitle(CallbackContext callbackContext, PackageManager packageManager){
     ApplicationInfo applicationInfo = null;
     try {
-      applicationInfo = packageManager.getApplicationInfo(this.cordova.getActivity().getApplicationContext()
-                                                                                    .getApplicationInfo()
-                                                                                    .packageName, 0);
+      applicationInfo = packageManager.getApplicationInfo(cordova.getActivity().getApplicationContext()
+        .getApplicationInfo()
+        .packageName, 0);
     } catch (NameNotFoundException e) {
       e.printStackTrace();
     }
 
     final String applicationName = (String) (applicationInfo != null ? packageManager.getApplicationLabel(applicationInfo) : "Unknown");
-        callbackContext.success(applicationName);
+    callbackContext.success(applicationName);
 
-        return true;
-	}
-	private void requestReviewInfo(){
-		Task<ReviewInfo> request = manager.requestReviewFlow();
-		request.addOnCompleteListener(task -> {
-			if (task.isSuccessful()) {
-				// We can get the ReviewInfo object
-				ReviewInfo reviewInfo = task.getResult();
-				this.launchReview(reviewInfo);
-			} else {
-				// There was some problem, continue regardless of the result.
-			}
-		});
-	}
-	private void launchReview(ReviewInfo reviewInfo ){
-		Task<Void> flow = manager.launchReviewFlow(this.cordova.getActivity(), reviewInfo);
-		flow.addOnCompleteListener(task -> {
-			// The flow has finished. The API does not indicate whether the user
-			// reviewed or not, or even whether the review dialog was shown. Thus, no
-			// matter the result, we continue our app flow.
-		});
-	}
+    return true;
+  }
+ 
+  private void reviewInApp(){
+    manager.requestReviewFlow().addOnCompleteListener(new OnCompleteListener<ReviewInfo>() {
+      @Override
+      public void onComplete(@NonNull Task<ReviewInfo> task) {
+        if(task.isSuccessful()){
+          reviewInfo = task.getResult();
+          manager.launchReviewFlow(cordova.getActivity(), reviewInfo).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+              Toast.makeText(cordova.getActivity(), "Rating Failed", Toast.LENGTH_SHORT).show();
+            }
+          }).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+              Toast.makeText(cordova.getActivity(), "Review Completed, Thank You!", Toast.LENGTH_SHORT).show();
+            }
+          });
+        }
+
+      }
+    }).addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(Exception e) {
+        Toast.makeText(cordova.getActivity(), "In-App Request Failed", Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
 
 }
